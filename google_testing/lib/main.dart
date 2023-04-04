04976f6176cbeef420c791b5a3db240dc1f39e15
Abdullah Alakeel <Abdullah.a.alakeel@boeing.com>
2023-04-04 16:36:00 +0300

    update

--- google_testing/lib/main.dart
+++ google_testing/lib/main.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:namer_app/playGround.dart';
 import 'riyadh_map.dart';
 
 void main() {
   runApp(MyApp());
+//   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//   statusBarColor: Colors.yellow
+// ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+            // // below system
+            // backgroundColor: Colors.yellow, // Status bar color
+            // systemOverlayStyle: SystemUiOverlayStyle(
+            //   statusBarColor: Colors.red, // Color for Android
+
+            //   // For iOS (dark icons)
+            //   statusBarBrightness: Brightness.light,
+
+            //   // For Android (dark icons)
+            //   statusBarIconBrightness: Brightness.light,
+            //   systemNavigationBarColor: Colors.green, // navigation bar color
+
+            // below system
+            backgroundColor: Colors.yellow, // Status bar color
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.white, // Color for Android
+              // For iOS (dark icons)
+              statusBarBrightness: Brightness.dark,
+              // For Android (dark icons)
+              statusBarIconBrightness: Brightness.dark,
+              systemNavigationBarContrastEnforced: false,
+              systemNavigationBarColor: Colors.white,
+              systemNavigationBarDividerColor: Colors.white,
+              systemNavigationBarIconBrightness:
+                  Theme.of(context).brightness == Brightness.light
+                      ? Brightness.dark
+                      : Brightness.light,
+            )),
+      ),
       title: 'Riyadh Map',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Riyadh Map'),
+          // brightness: Brightness.light, // or use Brightness.dark
         ),
-        body: RiyadhMap(),
+        // body: RiyadhMap(),
+        body: Home(),
       ),
     );
   }
---
+++ google_testing/lib/playGround.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
+import 'package:sms_autofill/sms_autofill.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _home createState() => _home();
+}
+
+class _home extends State<Home> {
+  final _nameFocus = FocusNode();
+  final _cardNumberFocus = FocusNode();
+  final _cardPinFocus = FocusNode();
+
+ @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SmsAutoFill().nregisterListener();
+    super.dispose();
+  }
+
+  KeyboardActionsConfig _buildKeyboardActionsConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      // keyboardBarColor: Colors.grey,
+
+      nextFocus: false,
+      actions: [
+        KeyboardActionsItem(
+          focusNode: _nameFocus,
+          onTapAction: () => {print('done clicked !!')},
+        ),
+        KeyboardActionsItem(
+          focusNode: _cardNumberFocus,
+        ),
+        KeyboardActionsItem(
+          focusNode: _cardPinFocus,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // return SingleChildScrollView(
+    return KeyboardActions(
+      config: _buildKeyboardActionsConfig(context),
+      child: Stack(children: [
+        Column(children: [
+          SizedBox(height: 10),
+          Text('Drop Validation'),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: DropdownSearch<int>(
+              items: [1, 2, 3, 4, 5, 6, 7],
+              autoValidateMode: AutovalidateMode.onUserInteraction,
+              onChanged: (item) => {print('selected => ${item}')},
+              validator: (int? i) {
+                if (i == null) return 'Required Filed';
+                // else if (i >= 5) return 'value should be < 5';
+                return null;
+              },
+              dropdownDecoratorProps: DropDownDecoratorProps(
+                dropdownSearchDecoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  // labelText: 'Users *',
+                  // filled: true,
+                  // fillColor: Theme.of(context).inputDecorationTheme.fillColor,
+                  hintText: "Select...",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  contentPadding: EdgeInsets.zero,
+                  // contentPadding: EdgeInsets.only(top: 12, left: 10),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+              popupProps: PopupProps.menu(
+                showSearchBox: true,
+              ),
+              clearButtonProps: ClearButtonProps(isVisible: true),
+            ),
+          ),
+          SizedBox(height: 60),
+          Text('Keyboard Test Validation'),
+          TextField(
+            decoration: const InputDecoration(labelText: "Name"),
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.text,
+            focusNode: _nameFocus,
+          ),
+          TextField(
+            decoration: const InputDecoration(labelText: "Card Number"),
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.number,
+            focusNode: _cardNumberFocus,
+          ),
+          TextField(
+            decoration: const InputDecoration(labelText: "Card Pin"),
+            textInputAction: TextInputAction.done,
+            keyboardType: TextInputType.number,
+            focusNode: _cardPinFocus,
+          ),
+        ]),
+      ]),
+    );
+  }
+}
